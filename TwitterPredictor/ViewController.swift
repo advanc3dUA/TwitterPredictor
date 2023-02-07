a0c935c24d005f17bf4f5259af7decaa100a5bbe
advanc3dUA <fash1k@gmail.com>
2023-02-07 11:10:18 +0100

    initial commit

---
+++ TwitterPredictor/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TwitterPredictor
+//
+//  Created by Yuriy Gudimov on 07.02.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var backgroundView: UIView!
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var sentimentLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+    @IBAction func predictPressed(_ sender: UIButton) {
+    }
+}
+
